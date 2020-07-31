059d4f0ec435436e6b5ba62a54770a2b3881955f
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-07-30 22:00:01 -0400

    I created the gamer computer factory

---
+++ src/com/giacomini/factories/GamerComputerFactory.java
@@ -0,0 +1,11 @@
+package com.giacomini.factories;
+
+import com.giacomini.products.Computer;
+
+public class GamerComputerFactory implements ComputerFactory {
+    @Override
+    public Computer createComputer() {
+        // TODO: create a new gamer computer
+        return null;
+    }
+}
