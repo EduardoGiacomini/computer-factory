083ab49bc86d07b88bbfab62f6c62e11133e62e8
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-07-30 22:19:08 -0400

    I created the gamer computer details factory

--- src/com/giacomini/factories/GamerComputerDetailsFactory.java
+++ src/com/giacomini/factories/GamerComputerDetailsFactory.java
@@ -1,4 +1,25 @@
 package com.giacomini.factories;
 
-public class GamerComputerDetailsFactory {
+import com.giacomini.products.GraphicCard;
+import com.giacomini.products.MotherBoard;
+import com.giacomini.products.Processor;
+
+public class GamerComputerDetailsFactory implements ComputerDetailsFactory {
+    @Override
+    public MotherBoard createMotherBoard() {
+        // TODO: create a new gamer mother board (maybe b450)
+        return null;
+    }
+
+    @Override
+    public Processor createProcessor() {
+        // TODO: create a new gamer processor (maybe ryzen 5 3600x)
+        return null;
+    }
+
+    @Override
+    public GraphicCard createGraphicCard() {
+        // TODO: create a new gamer graphic card (maybe rtx 590 8gb)
+        return null;
+    }
 }
